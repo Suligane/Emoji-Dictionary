1224281ae289f7cb71b24bc01f87dfa7d97981b0
Unknown <kevin.sauves@me.com>
2018-03-03 19:35:11 -0500

    Emoji Dictionary
    Segue is working, Tapping works, s

---
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiDefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Kevin Sauves on 2018-03-03.
+//  Copyright © 2018 Kevin Sauves. All rights reserved.
+//
+
+import UIKit
+
+class EmojiDefinitionViewController: UIViewController {
+    
+    var emoji = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(emoji)
+
+    }
+}
---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Kevin Sauves on 2018-03-03.
+//  Copyright © 2018 Kevin Sauves. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+   var emojis = ["😀","💩", "🏎", "🕍", "🥑"]
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return the number of rows we want to have in the tableview.
+        return emojis.count
+        
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "ourSegue", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        let emojiDefVC = segue.destination as! EmojiDefinitionViewController
+        emojiDefVC.emoji = "We did it!"
+        
+    }
+
+}
